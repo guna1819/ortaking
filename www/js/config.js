0a43631ba4f6c433ce14c75181b7ca70fdd6baf1
guna <guna@gunas-MBP.Dlink>
2018-03-07 19:17:10 +0530

    First commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "https://weborder.chennaiexpress.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Chennai Express Order Taking App",
+	'pushNotificationSenderid' : "507008644414",
+	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
+};
