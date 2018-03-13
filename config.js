1cda609ef40b4d8972859f40a7a5e2d0daba870c
guna1819 <37141457+guna1819@users.noreply.github.com>
2018-03-13 13:52:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={			
-	'ApiUrl' : "https://weborder.chennaiexpress.com/merchantapp/api",	
+	'ApiUrl' : "http://weborder.chennaiexpress.com/merchantapp/api",	
 	'DialogDefaultTitle' : "Chennai Express Order Taking App",
 	'pushNotificationSenderid' : "507008644414",
 	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
