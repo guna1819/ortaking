72ffb6375465b244b319811b1294a27c59911e2f
guna1819 <37141457+guna1819@users.noreply.github.com>
2018-03-13 13:41:51 +0530

    Add files via upload

---
+++ www/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "https://weborder.chennaiexpress.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Chennai Express Order Taking App",
+	'pushNotificationSenderid' : "507008644414",
+	'APIHasKey' : "2355c7c0ab8097a39b78b8ea2a02b27d"
+};
